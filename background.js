efe6d09a1523b2b80669685f3d35996b78a79130
Yuvraj IND <y.india.main@gmail.com>
2026-08-16 14:29:01 +0530

    try new zip

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.commands.onCommand.addListener(async (command) => {
-  if (command !== 'open-extension') return;
-  try {
-    await chrome.action.openPopup();
-  } catch (error) {
-    console.warn('Could not open popup from command:', error);
-  }
-});
